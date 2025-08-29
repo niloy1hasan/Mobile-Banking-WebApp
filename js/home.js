6d5f0e1e7cb55e946f6534f4c57cd40888881732
niloy1hasan <notecse23@gmail.com>
2025-08-29 08:44:21 +0600

    cashout money section added

--- js/home.js
+++ js/home.js
@@ -1,3 +1,14 @@
+/* start declaration part */
+
+/* menu button section */
+const menuBtnAddMoney = document.getElementById('add-money-menu-btn');
+const menuBtnCashout = document.getElementById('cashout-menu-btn');
+
+/* section's section */
+const addMoneySection = document.getElementById('add-money-section');
+const cashoutSection = document.getElementById('cashout-section');
+
+/* add money section */
 const addMoneyBtn = document.getElementById('add-money-btn');
 const userAccountNumber = document.getElementById('bank-account-number');
 const userPin = document.getElementById('user-pin');
@@ -5,23 +16,69 @@ const userAddMoneyAmount = document.getElementById('money-amount');
 const userSelectedBank = document.getElementById('user-selected-bank');
 const userBalance = document.getElementById('balanceAmount');
 
-const account = '01234567891';
+/* cashout section */
+const agentNumber = document.getElementById('agent-number');
+const cashoutAmount = document.getElementById('cashout-amount');
+const cashoutUserPIN = document.getElementById('cashout-user-pin');
+const withdrawBtn = document.getElementById('withdraw-btn');
+
+/* end declaration part */
+// 
+/* start function part */
+
+function toggleMenu(visibleItem){
+    addMoneySection.style.display = 'none';
+    cashoutSection.style.display = 'none';
+
+    visibleItem.style.display = 'block';
+    visibleItem.cl = 'block';
+    visibleItem.style.display = 'block';
+}
+
+
+function userAuth(accountNumber, password){
+const account = '00123456789';
 const pin = '1234';
 
+if(accountNumber!==account){
+        alert("Account Number not found");
+        return false;
+    } else if(password!==pin){
+        alert("Pin is wrong");
+        return false;
+    }
+
+return (account===accountNumber && password===pin);
+}
+
+/* end function part */
+// 
+/* start event listener part */
+
+/* menu button */
+
+menuBtnAddMoney.addEventListener("click", function(event){
+    event.preventDefault();
+    toggleMenu(addMoneySection);
+});
+
+menuBtnCashout.addEventListener("click", function(event){
+    event.preventDefault();
+    toggleMenu(cashoutSection);
+});
+    
+
+
+
+/* add money */
 addMoneyBtn.addEventListener("click", function(event){
     event.preventDefault();
     
-    givenAccountNumber = userAccountNumber.value;
-    giverUserPin = userPin.value;
+    let givenAccountNumber = userAccountNumber.value;
+    let giverUserPin = userPin.value;
+    if(!userAuth(givenAccountNumber, giverUserPin)) return;
 
-    if(givenAccountNumber!==account){
-        alert("Account Number not found");
-        return;
-    } else if(giverUserPin!==pin){
-        alert("Pin is wrong");
-        return;
-    }
-
+    
     let amount = parseInt(userAddMoneyAmount.value);
     if(amount>0){
     amount += parseInt(userBalance.innerText);
@@ -32,6 +89,26 @@ addMoneyBtn.addEventListener("click", function(event){
     } else {
         alert("Given amount is not valid");
     }
-
-    
 });
+
+/* cashout money */
+withdrawBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    let givenAccountNumber = agentNumber.value; 
+    let giverUserPin = cashoutUserPIN.value;
+    if(!userAuth(givenAccountNumber, giverUserPin)) return;
+
+    let amount = parseInt(userBalance.innerText);
+    if(amount>0){
+    amount -= parseInt(cashoutAmount.value);
+    userBalance.innerText = amount;
+    agentNumber.value = "";
+    cashoutUserPIN.value = "";
+    cashoutAmount.value = "";
+    } else {
+        alert("Given amount is not valid");
+    }
+});
+
+
+/* end event listener part */
